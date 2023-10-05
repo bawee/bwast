63cd7ead6d27378b4a48dc41d2dc4f6080653dc2
bawee <bawee@hotmail.com>
2023-10-05 09:54:40 +0100

    Improve output tab file formatting

--- bwast.py
+++ bwast.py
@@ -105,7 +105,9 @@ def doBlast(inputList):
         blastOptions = re.sub(r"\s+", r".", blastOptions)
         if args.verbose: print("with options: %s" % (blastOptionsPre))
         
-        blast_out = "%s.vs.%s.%s.%s.tab" % (queryName, subjecName, blastOptions, blastType)
+        # Constructing the filename
+        blast_out_parts = [queryName, subjecName, blastOptions, blastType]
+        blast_out = ".".join(part for part in blast_out_parts if part) + ".tab"
 
         actList.append(blast_out) #append blast file to ACT input list
         
